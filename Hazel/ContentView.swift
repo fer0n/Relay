1c64f599a8c3b4ec4c33a443e7721df1c2285280
fer0n <rnichi@me.com>
2026-07-13 18:40:02 +0200

    Initial Commit

---
+++ Hazel/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Hazel
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
